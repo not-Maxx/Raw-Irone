bac596ad9f1b9cdfce749e30922fed7ae315f776
maxx <iXtreme9990@gmail.com>
2022-06-07 10:05:32 -0700

    Updated various files

---
+++ src/main/java/maxx/raw/irone/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package maxx.raw.irone.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import maxx.raw.irone.RawIrone;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		RawIrone.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
