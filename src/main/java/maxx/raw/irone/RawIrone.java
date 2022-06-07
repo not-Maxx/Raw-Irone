06166f235d361f2be6e4dee24c3111f4783bcd03
maxx <iXtreme9990@gmail.com>
2022-06-07 10:05:18 -0700

    Added Raw Irone

---
+++ src/main/java/maxx/raw/irone/RawIrone.java
@@ -0,0 +1,31 @@
+package maxx.raw.irone;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RawIrone implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	public static final Item RAW_IRONE = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registry.ITEM, new Identifier("rawirone", "raw_irone"), RAW_IRONE);
+	}
+}
